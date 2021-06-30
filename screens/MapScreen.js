13fb0a99ed481b65c860ce04d28bd562ff99ec3f
ajayg415 <ajay.g415@gmail.com>
2021-06-30 16:23:27 +0530

    added navigation and basic screens setup

---
+++ screens/MapScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text, StyleSheet } from "react-native";
+
+const MapScreen = () => {
+    return <View><Text>MapScreen</Text></View> 
+}
+
+const styles = StyleSheet.create({}) 
+
+export default MapScreen
