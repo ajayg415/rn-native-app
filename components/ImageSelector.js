287a581414cc42f96ea02835469d034b914ca766
ajayg415 <ajay.g415@gmail.com>
2021-07-01 07:16:51 +0530

    added picked image on screen

--- components/ImageSelector.js
+++ components/ImageSelector.js
@@ -1,33 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Alert, View, Button, Text, StyleSheet, Image } from "react-native";
-import * as ImagePicker from 'expo-image-picker'
-import * as Permission from 'expo-permissions'
+import * as ImagePicker from "expo-image-picker";
+import * as Permission from "expo-permissions";
 
 import Colors from "../constants/Colors";
 
 const ImgPicker = ({}) => {
+  const [pickedImg, setPickedImg] = useState();
 
-    const verifyPermissions = async () => {
-        const res = await Permission.askAsync(Permission.CAMERA)
-        if(res.status !== 'granted') {
-            Alert.alert('Insufficient permissions', 'You need to grant permissions', [{text: 'okay'}])
-            return false
-        }
-        return true 
+  const verifyPermissions = async () => {
+    const res = await Permission.askAsync(Permission.CAMERA);
+    if (res.status !== "granted") {
+      Alert.alert("Insufficient permissions", "You need to grant permissions", [
+        { text: "okay" },
+      ]);
+      return false;
     }
+    return true;
+  };
 
   const takeImageHandler = async () => {
-    if(await verifyPermissions()){
-        ImagePicker.launchCameraAsync()
+    if (await verifyPermissions()) {
+      const image = await ImagePicker.launchCameraAsync({
+        allowsEditing: true,
+        aspect: [16, 9],
+        quality: 0.5,
+      });
+      setPickedImg(image.uri);
     }
-    
   };
 
   return (
     <View style={styles.imagePicker}>
       <View style={styles.imagePreview}>
-        <Text>No image picked yet</Text>
-        <Image style={styles.image} />
+        {pickedImg ? (
+          <Image style={styles.image} source={{ uri: pickedImg }} />
+        ) : (
+          <Text>No image picked yet</Text>
+        )}
       </View>
       <Button
         title="Take Image"
