4508b79415deaa40f37a38522378c949ff0a0725
ajayg415 <ajay.g415@gmail.com>
2021-07-01 07:44:35 +0530

    moved the taken image into permanent storage facility

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,12 +1,27 @@
+import * as FileSystem from "expo-file-system";
+
 export const ADD_PLACE = "ADD_PLACE";
 
 export const addPlace = (title, image) => {
-  console.log({ title });
-  return {
-    type: ADD_PLACE,
-    placeData: {
-      title,
-      image,
-    },
+  return async (dispatch) => {
+    const fileName = image.split("/").pop();
+    const newPath = FileSystem.documentDirectory + fileName;
+    try {
+      await FileSystem.moveAsync({
+        from: image,
+        to: newPath,
+      });
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+
+    dispatch({
+      type: ADD_PLACE,
+      placeData: {
+        title,
+        image: newPath,
+      },
+    });
   };
 };
